50f739038d62c8bd0c306ba32bee724b7c911a6a
Michael Lombard <miklombard@yahoo.com>
2023-05-25 08:40:13 -0400

    pair lab

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 // Write your code here!
 main.remove();
-
-const newHeader = document.createElement("h1");
-newHeader.setAttribute("id", "victory");
-newHeader.textContent = "Jerry is the champion";
-
+const  newHeader = document.createElement('h1');
+newHeader.id = 'victory'
+newHeader.textContent = 'Yu is the champion';
 document.body.append(newHeader);
